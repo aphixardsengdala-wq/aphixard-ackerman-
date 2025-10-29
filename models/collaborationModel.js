1562b6f1df0ac2e99f7e92bc42bd43c6badc3028
bee-15 <aphixardsengdala@gmail.com>
2025-10-29 18:50:10 +0700

    haaaaa

--- models/collaborationModel.js
+++ models/collaborationModel.js
@@ -4,7 +4,8 @@ const dataCollaborations = [
     { collaboration_task: "help monster in cave demon" },
     { collaboration_assign: "support" },
     { collaboration_comment: "verry good all" },
-    { collaboration_progress: "Everything is going well" }
+    { collaboration_progress: "Everything is going well" },
+    { collaboration_Greateday: "today i will to going work"}
 ];
 
 const collaborationModel = {
